54f408954483831df10f821d99b3adc818866bd7
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-11 16:18:13 +0530

    Refactor: UC1-UC4

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t \t \t \t \t Welcome to the Address Book Program");
+            while (true)
+            {
+
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        AddressBookDetails.AddMember();
+                        break;
+                    case 2:
+                        AddressBookDetails.ViewContacts();
+                        break;
+                    case 3:
+                        AddressBookDetails.EditDetails();
+                        break;
+                    case 4:
+                        AddressBookDetails.DeleteDetails();
+                        break;
+                    case 5:
+                        // to exit from main method
+                        Console.WriteLine("Exited");
+                        return;
+
+                }
+
+            }
+        }
+    }
+}
